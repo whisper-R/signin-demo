d766d1ee8be7c7fcc34e821daa6bbc71c39d1d45
whisper-R <75459002+whisper-R@users.noreply.github.com>
2023-07-19 22:38:04 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,46 +1,3 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"io/ioutil"
-// 	"net/http"
-// 	"os"
-// )
-
-// func main() {
-// 	url := "https://api.xiabb.chat/chatapi/marketing/signin"
-
-// 	req, err := http.NewRequest("POST", url, nil)
-// 	if err != nil {
-// 		fmt.Println("Failed to create request:", err)
-// 		return
-// 	}
-// 	Authorization := os.Getenv("AUTHORIZATION")
-// 	if Authorization == "" {
-// 		fmt.Println("Authorization不存在，请检查是否添加")
-// 		return
-// 	}
-// 	req.Header.Set("Authorization", Authorization)
-
-// 	client := &http.Client{}
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		fmt.Println("Failed to send request:", err)
-// 		return
-// 	}
-// 	defer resp.Body.Close()
-
-// 	fmt.Println("Response Status:", resp.Status)
-
-// 	body, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		fmt.Println("Failed to read response body:", err)
-// 		return
-// 	}
-
-// 	fmt.Println("Response Body:", string(body))
-// }
-
 package main
 
 import (
@@ -48,54 +5,97 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
 )
 
 func main() {
 	url := "https://api.xiabb.chat/chatapi/marketing/signin"
 
-	authorizationEnv := os.Getenv("AUTHORIZATIONS")
-	fmt.Println("authorizationEnv:", authorizationEnv)
-	authorizations := strings.Split(authorizationEnv, ",")
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		fmt.Println("Failed to create request:", err)
+		return
+	}
+	Authorization := os.Getenv("AUTHORIZATION")
+	if Authorization == "" {
+		fmt.Println("Authorization不存在，请检查是否添加")
+		return
+	}
+	req.Header.Set("Authorization", Authorization)
 
 	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Failed to send request:", err)
+		return
+	}
+	defer resp.Body.Close()
 
-	for _, authorization := range authorizations {
-		if authorization == "" {
-			fmt.Println("Authorization不存在，请检查是否添加")
-			continue
-		}
+	fmt.Println("Response Status:", resp.Status)
 
-		req, err := http.NewRequest("POST", url, nil)
-		if err != nil {
-			fmt.Println("Failed to create request:", err)
-			return
-		}
-
-		req.Header.Set("Authorization", authorization)
-
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println("Failed to send request:", err)
-			return
-		}
-		defer resp.Body.Close()
-
-		fmt.Println("Response Status:", resp.Status)
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("Failed to read response body:", err)
-			return
-		}
-
-		fmt.Println("Response Body:", string(body))
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Failed to read response body:", err)
+		return
 	}
 
-	if len(authorizations) == 1 && authorizations[0] == authorizationEnv {
-		fmt.Println("只找到一个Authorization:", authorizations[0])
-	}
+	fmt.Println("Response Body:", string(body))
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"io/ioutil"
+// 	"net/http"
+// 	"os"
+// 	"strings"
+// )
+
+// func main() {
+// 	url := "https://api.xiabb.chat/chatapi/marketing/signin"
+
+// 	authorizationEnv := os.Getenv("AUTHORIZATIONS")
+// 	fmt.Println("authorizationEnv:", authorizationEnv)
+// 	authorizations := strings.Split(authorizationEnv, ",")
+
+// 	client := &http.Client{}
+
+// 	for _, authorization := range authorizations {
+// 		if authorization == "" {
+// 			fmt.Println("Authorization不存在，请检查是否添加")
+// 			continue
+// 		}
+
+// 		req, err := http.NewRequest("POST", url, nil)
+// 		if err != nil {
+// 			fmt.Println("Failed to create request:", err)
+// 			return
+// 		}
+
+// 		req.Header.Set("Authorization", authorization)
+
+// 		resp, err := client.Do(req)
+// 		if err != nil {
+// 			fmt.Println("Failed to send request:", err)
+// 			return
+// 		}
+// 		defer resp.Body.Close()
+
+// 		fmt.Println("Response Status:", resp.Status)
+
+// 		body, err := ioutil.ReadAll(resp.Body)
+// 		if err != nil {
+// 			fmt.Println("Failed to read response body:", err)
+// 			return
+// 		}
+
+// 		fmt.Println("Response Body:", string(body))
+// 	}
+
+// 	if len(authorizations) == 1 && authorizations[0] == authorizationEnv {
+// 		fmt.Println("只找到一个Authorization:", authorizations[0])
+// 	}
+// }
 
 
 
