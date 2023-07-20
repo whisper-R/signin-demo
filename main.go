5057a9ed6904017c44f60b94a79221c8e85d8bdb
whisper-R <75459002+whisper-R@users.noreply.github.com>
2023-07-20 12:44:57 +0800

    Update main.go
    多用户

--- main.go
+++ main.go
@@ -1,3 +1,48 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"io/ioutil"
+// 	"net/http"
+// 	"os"
+// )
+
+// func main() {
+// 	url := "https://api.xiabb.chat/chatapi/marketing/signin"
+
+// 	req, err := http.NewRequest("POST", url, nil)
+// 	if err != nil {
+// 		fmt.Println("Failed to create request:", err)
+// 		return
+// 	}
+// 	Authorization := os.Getenv("AUTHORIZATION")
+	
+// 	if Authorization == "" {
+// 		fmt.Println("Authorization不存在，请检查是否添加")
+// 		return
+// 	}
+// 	req.Header.Set("Authorization", Authorization)
+
+// 	client := &http.Client{}
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		fmt.Println("Failed to send request:", err)
+// 		return
+// 	}
+// 	defer resp.Body.Close()
+
+// 	fmt.Println("Response Status:", resp.Status)
+
+// 	body, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		fmt.Println("Failed to read response body:", err)
+// 		return
+// 	}
+
+// 	fmt.Println("Response Body:", string(body))
+// }
+
+
 package main
 
 import (
@@ -10,93 +55,36 @@ import (
 func main() {
 	url := "https://api.xiabb.chat/chatapi/marketing/signin"
 
-	req, err := http.NewRequest("POST", url, nil)
-	if err != nil {
-		fmt.Println("Failed to create request:", err)
-		return
-	}
-	Authorization := os.Getenv("AUTHORIZATION")
-	
-	if Authorization == "" {
-		fmt.Println("Authorization不存在，请检查是否添加")
-		return
-	}
-	req.Header.Set("Authorization", Authorization)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Failed to send request:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("Response Status:", resp.Status)
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Failed to read response body:", err)
-		return
+	authorizations := []string{
+		os.Getenv("AUTHORIZATION_1"),
+		os.Getenv("AUTHORIZATION_2"),
 	}
 
-	fmt.Println("Response Body:", string(body))
+	for i, authorization := range authorizations {
+		username := fmt.Sprintf("用户%d", i+1)
+		req, err := http.NewRequest("POST", url, nil)
+		if err != nil {
+			fmt.Println("Failed to create request:", err)
+			return
+		}
+		req.Header.Add("Authorization", authorization)
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println("Failed to send request:", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		fmt.Printf("Response Status for %s: %s\n", username, resp.Status)
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Failed to read response body:", err)
+			return
+		}
+
+		fmt.Printf("Response Body for %s: %s\n", username, string(body))
+	}
 }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"io/ioutil"
-// 	"net/http"
-// 	"os"
-// 	"strings"
-// )
-
-// func main() {
-// 	url := "https://api.xiabb.chat/chatapi/marketing/signin"
-
-// 	authorizationEnv := os.Getenv("AUTHORIZATIONS")
-// 	fmt.Println("authorizationEnv:", authorizationEnv)
-// 	authorizations := strings.Split(authorizationEnv, ",")
-
-// 	client := &http.Client{}
-
-// 	for _, authorization := range authorizations {
-// 		if authorization == "" {
-// 			fmt.Println("Authorization不存在，请检查是否添加")
-// 			continue
-// 		}
-
-// 		req, err := http.NewRequest("POST", url, nil)
-// 		if err != nil {
-// 			fmt.Println("Failed to create request:", err)
-// 			return
-// 		}
-
-// 		req.Header.Set("Authorization", authorization)
-
-// 		resp, err := client.Do(req)
-// 		if err != nil {
-// 			fmt.Println("Failed to send request:", err)
-// 			return
-// 		}
-// 		defer resp.Body.Close()
-
-// 		fmt.Println("Response Status:", resp.Status)
-
-// 		body, err := ioutil.ReadAll(resp.Body)
-// 		if err != nil {
-// 			fmt.Println("Failed to read response body:", err)
-// 			return
-// 		}
-
-// 		fmt.Println("Response Body:", string(body))
-// 	}
-
-// 	if len(authorizations) == 1 && authorizations[0] == authorizationEnv {
-// 		fmt.Println("只找到一个Authorization:", authorizations[0])
-// 	}
-// }
-
-
-
